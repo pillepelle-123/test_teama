7ccaa1a21f53f01a5578201906a56af81462fc8b
Peter Stedeler <pstedeler@AD.CIMT.DE>
2021-02-05 18:11:29 +0100

    Initial commit of the project

---
+++ libraryA/src/main/java/com/company/projectA/App.java
@@ -0,0 +1,13 @@
+package com.company.projectA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello A2" );
+    }
+}
