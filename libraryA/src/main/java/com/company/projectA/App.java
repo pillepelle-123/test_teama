1836e72f11a41fe270bb8423ff52503ea70c6c05
Peter Stedeler <pstedeler@AD.CIMT.DE>
2021-02-08 14:21:48 +0100

    Kasdadierung testen

--- libraryA/src/main/java/com/company/projectA/App.java
+++ libraryA/src/main/java/com/company/projectA/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins, kaskadiere mal bitte!" );
+        System.out.println( "Hello Jenkins, kaskadiere mal bitte jetzt aber richtig!" );
     }
 }
